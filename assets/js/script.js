412e99a677b0a9af9deb22417f67049d51c80129
MD. MONWAR SIDDIQ <monwarsiddiq@gmail.com>
2021-11-12 19:18:46 +0600

    Home Page v1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,35 +5,42 @@ function scrollFunction() {
     document.getElementById("header").style.padding = "0px"
     document.getElementById("header").style.backgroundColor = "#F2994A"
   } else {
-    document.getElementById("header").style.padding = "10px 0px"
+    document.getElementById("header").style.padding = "5px"
     document.getElementById("header").style.backgroundColor = "#F2994A"
   }
 }
 
 $('.hero-slider').owlCarousel({
-  loop:true,
+  loop: true,
   items:1,
   dots: false,
   nav: false,
-  autoplay:true,
-  autoplayTimeout:3000
+  autoplay: true,
+  autoplayTimeout: 3000
 });
 
 $('.owl-carousel').owlCarousel({
   loop:true,
-  margin:30,
+  margin:20,
   dots: true,
   nav:false,
   responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:4
-      }
+    0:{
+        items:1
+    },
+    600:{
+        items:3
+    },
+    1000:{
+        items:4
+    }
   }
 });
 
+$('.item').hover(function() {
+  $(this).find('.add-basket-btn').show();
+},
+function () {
+   $('.add-basket-btn').hide();
+}
+);
